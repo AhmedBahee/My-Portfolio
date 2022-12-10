de637504fbda7ae57a5658f2eda98b78ba8ce57c
AhmedBahee <ahmed.b.e@hotmail.com>
2022-12-10 11:26:02 +0100

    test

--- js/main.js
+++ js/main.js
@@ -23,7 +23,6 @@ function viewCv() {
   );
 }
 
-/*دا خاص بتقليب صور المشروع */
 function changeImage(element) {
   var main_prodcut_image = document.getElementById("main_product_image");
   main_prodcut_image.src = element.src;
